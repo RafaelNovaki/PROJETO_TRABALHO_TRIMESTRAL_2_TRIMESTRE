c02284e83bb270b3d786859fcade9e2a02989fe2
RafaelNovaki <159036779+RafaelNovaki@users.noreply.github.com>
2025-08-02 16:59:23 -0300

    Update historico.js

--- PROJETO/js/historico.js
+++ PROJETO/js/historico.js
@@ -1,6 +1,4 @@
-// js/historico.js
-
-// Por enquanto não há funcionalidades específicas, apenas código placeholder
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Página Histórico carregada.');
 });
+
